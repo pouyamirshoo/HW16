f023170f653430793bb9cd34c8176bf13fb37eff
pc <poyatajhiz59@gmail.com>
2024-04-07 14:32:25 -0800

    creat and fill BaseService interface

---
+++ src/main/java/base/service/BaseService.java
@@ -0,0 +1,16 @@
+package base.service;
+
+import base.entity.BaseEntity;
+
+import java.io.Serializable;
+
+public interface BaseService<T extends BaseEntity<ID>,ID extends Serializable> {
+    T saveOrUpdate(T entity);
+
+    T findById(ID id);
+
+    void delete(T t);
+
+    T findByUsername(String username);
+    T findByUsernameAndPassword(String username,String password);
+}
