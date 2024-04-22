6e427aa4b0774d2a3b970f798065fca893c2f30a
pc <poyatajhiz59@gmail.com>
2024-04-22 15:35:20 -0800

    update BaseService class

--- src/main/java/base/service/BaseService.java
+++ src/main/java/base/service/BaseService.java
@@ -5,12 +5,12 @@ import base.entity.BaseEntity;
 import java.io.Serializable;
 
 public interface BaseService<T extends BaseEntity<ID>,ID extends Serializable> {
-    T saveOrUpdate(T entity);
+    T saveOrUpdate(T entity) throws IllegalStateException;
+
 
     T findById(ID id);
 
     void delete(T t);
+    boolean validate(T entity);
 
-    T findByUsername(String username);
-    T findByUsernameAndPassword(String username,String password);
 }
