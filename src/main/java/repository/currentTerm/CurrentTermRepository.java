4092ef0e46e51ad78077900f3467e76f15b418f5
pc <poyatajhiz59@gmail.com>
2024-04-22 15:52:53 -0800

    creat CurrentTermRepository class

---
+++ src/main/java/repository/currentTerm/CurrentTermRepository.java
@@ -0,0 +1,8 @@
+package repository.currentTerm;
+
+import base.repository.BaseRepository;
+import model.Current;
+
+public interface CurrentTermRepository extends BaseRepository<Current,Long> {
+
+}
