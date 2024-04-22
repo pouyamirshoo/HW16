2dcf445f235c3e888e811cc569a4d864ef30c44c
pc <poyatajhiz59@gmail.com>
2024-04-22 15:42:02 -0800

    creat TeacherRepository class

---
+++ src/main/java/repository/teacher/TeacherRepository.java
@@ -0,0 +1,15 @@
+package repository.teacher;
+
+import base.repository.BaseRepository;
+import jakarta.persistence.NoResultException;
+import model.Term;
+import model.person.Teacher;
+
+import java.util.List;
+
+public interface TeacherRepository extends BaseRepository<Teacher,Long> {
+
+     Teacher findByUserAndPass(String username,String password) throws NoResultException;
+     Teacher findByFullName(String firstName, String lastName) throws NoResultException;
+     Term findByYearAndHalfYear(int year,int halfYear) throws NoResultException;
+}
