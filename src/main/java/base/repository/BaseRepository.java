87ed0233493d1a5017a971a5c2c261c6b00bed59
pc <poyatajhiz59@gmail.com>
2024-04-22 15:34:46 -0800

    update BaseRepository class

--- src/main/java/base/repository/BaseRepository.java
+++ src/main/java/base/repository/BaseRepository.java
@@ -7,9 +7,7 @@ import java.util.Optional;
 
 public interface BaseRepository <T extends BaseEntity<ID>,ID extends Serializable> {
 
-    T saveOrUpdate(T entity);
+    T saveOrUpdate(T entity) throws IllegalStateException;
     Optional<T> findById(ID id);
     void delete(T entity);
-    Optional<T> findByUsername(String username);
-    Optional<T> findByUsernameAndPassword(String username,String password);
 }
