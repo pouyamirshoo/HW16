e40c26149c578bc75e77f94ff7ef3674fe901be3
pc <poyatajhiz59@gmail.com>
2024-04-22 15:34:22 -0800

    creat Current model class

---
+++ src/main/java/model/Current.java
@@ -0,0 +1,45 @@
+package model;
+
+import base.entity.BaseEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+import model.person.Student;
+import org.hibernate.validator.constraints.Range;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+public class Current extends BaseEntity<Long> {
+    @NotNull
+    int year;
+    @NotNull(message = "please enter the half term")
+    @Range(min = 1,max = 2,message = "half term number must be 1 or 2")
+    int halfTerm;
+    @OneToOne
+    @JoinColumn(referencedColumnName = "id")
+    Student student;
+    @ManyToMany(cascade = CascadeType.MERGE)
+    List<Lesson> lessons;
+
+    public Current(int year, int halfTerm) {
+        this.year = year;
+        this.halfTerm = halfTerm;
+    }
+//    public void addLesson(Lesson lesson) {
+//        if (lesson != null) {
+//            lessons.add(lesson);
+//            lesson.getCurrents().add(this);
+//        }
+    }
+
