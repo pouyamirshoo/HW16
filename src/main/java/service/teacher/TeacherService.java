fe9021dd28339315435d5a0c0b05a7d6da11c28e
pc <poyatajhiz59@gmail.com>
2024-04-23 13:13:53 -0800

    update TeacherService class

--- src/main/java/service/teacher/TeacherService.java
+++ src/main/java/service/teacher/TeacherService.java
@@ -5,8 +5,12 @@ import jakarta.persistence.NoResultException;
 import model.Term;
 import model.person.Teacher;
 
-public interface TeacherService extends BaseService<Teacher,Long> {
-    Teacher findByUserAndPass(String username,String password) throws NoResultException;
+import java.util.List;
+
+public interface TeacherService extends BaseService<Teacher, Long> {
+    Teacher findByUserAndPass(String username, String password) throws NoResultException;
+
     Teacher findByFullName(String firstName, String lastName) throws NoResultException;
+
     Term findByYearAndHalfYear(int year, int halfYear) throws NoResultException;
 }
