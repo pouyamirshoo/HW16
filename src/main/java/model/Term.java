39574f2a967374a3175979dd345f126f241452dc
pc <poyatajhiz59@gmail.com>
2024-04-23 13:11:40 -0800

    update Term model class

--- src/main/java/model/Term.java
+++ src/main/java/model/Term.java
@@ -4,7 +4,6 @@ import base.entity.BaseEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
@@ -19,13 +18,13 @@ import org.hibernate.validator.constraints.Range;
 @Entity
 public class Term extends BaseEntity<Long> {
     @NotNull(message = "please enter year of term")
-    @Range(min = 1375,max= 1402,message = "year of term must be between 1375 - 1402")
+    @Range(min = 1375, max = 1402, message = "year of term must be between 1375 - 1402")
     int yearOfTerm;
     @NotNull(message = "please enter the half term")
-    @Range(min = 1,max = 2,message = "half term number must be 1 or 2")
+    @Range(min = 1, max = 2, message = "half term number must be 1 or 2")
     int halfTerm;
     @NotNull(message = "please enter the count Of Lessons")
-    @Range(min = 12,max = 29,message = "teachers can not get less than 12 units or more than 29")
+    @Range(min = 12, max = 29, message = "teachers can not get less than 12 units or more than 29")
     int countOfLessons;
     @ManyToOne
     @JoinColumn(referencedColumnName = "id")
