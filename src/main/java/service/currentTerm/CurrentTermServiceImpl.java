d11befcc8033cf2ffca84ffe0468f23ff0c0d365
pc <poyatajhiz59@gmail.com>
2024-04-23 13:07:09 -0800

    add findAllCurrentTerms method to CurrentTermServiceImpl class

--- src/main/java/service/currentTerm/CurrentTermServiceImpl.java
+++ src/main/java/service/currentTerm/CurrentTermServiceImpl.java
@@ -5,10 +5,16 @@ import model.Current;
 import org.hibernate.SessionFactory;
 import repository.currentTerm.CurrentTermRepository;
 
+import java.util.List;
+
 public class CurrentTermServiceImpl extends BaseServiceImpl<Current, Long, CurrentTermRepository>
         implements CurrentTermService {
     public CurrentTermServiceImpl(CurrentTermRepository repository, SessionFactory sessionFactory) {
         super(repository, sessionFactory);
     }
 
+    @Override
+    public List<Current> findAllCurrentTerms() {
+        return repository.findAllCurrentTerms();
+    }
 }
