ef76abc56f82f529ebd25f5fd8f5bf2711cf9f44
pc <poyatajhiz59@gmail.com>
2024-04-23 13:12:39 -0800

    update TeacherRepository class

--- src/main/java/repository/teacher/TeacherRepository.java
+++ src/main/java/repository/teacher/TeacherRepository.java
@@ -5,10 +5,13 @@ import jakarta.persistence.NoResultException;
 import model.Term;
 import model.person.Teacher;
 
+import java.util.List;
+
 
 public interface TeacherRepository extends BaseRepository<Teacher,Long> {
 
      Teacher findByUserAndPass(String username,String password) throws NoResultException;
      Teacher findByFullName(String firstName, String lastName) throws NoResultException;
      Term findByYearAndHalfYear(int year,int halfYear) throws NoResultException;
+     List<Term> findByStudentId(Long id) throws NoResultException;
 }
