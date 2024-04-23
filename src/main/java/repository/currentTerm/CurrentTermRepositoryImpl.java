ece7eefefec2523544641276edc93ed92da98d79
pc <poyatajhiz59@gmail.com>
2024-04-23 13:04:53 -0800

    add findAllCurrentTerms method to CurrentTermRepositoryImpl class

--- src/main/java/repository/currentTerm/CurrentTermRepositoryImpl.java
+++ src/main/java/repository/currentTerm/CurrentTermRepositoryImpl.java
@@ -1,8 +1,14 @@
 package repository.currentTerm;
 
 import base.repository.BaseRepositoryImpl;
+import conncetion.SessionFactorySingleton;
 import model.Current;
+
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
+import java.util.List;
 
 public class CurrentTermRepositoryImpl extends BaseRepositoryImpl<Current, Long>
         implements CurrentTermRepository {
@@ -15,4 +21,13 @@ public class CurrentTermRepositoryImpl extends BaseRepositoryImpl<Current, Long>
         return Current.class;
     }
 
+    @Override
+    public List<Current> findAllCurrentTerms() {
+        String hql = "FROM Current ";
+        Session session = SessionFactorySingleton.getInstance().openSession();
+        Query<Current> query = session.createQuery(hql);
+        List<Current> currents = query.list();
+        session.close();
+        return currents;
+    }
 }
