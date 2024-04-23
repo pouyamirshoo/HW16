7682ce8b4f7b043ae6bb869762df0f066ca2498e
pc <poyatajhiz59@gmail.com>
2024-04-23 13:10:22 -0800

    update Employee model class

--- src/main/java/model/person/Employee.java
+++ src/main/java/model/person/Employee.java
@@ -14,20 +14,13 @@ import lombok.experimental.FieldDefaults;
 @Entity
 public class Employee extends Person {
 
-     int salary;
-    public Employee(Long id){
-        this.id = id;
-    }
-
-    public Employee(int salary) {
-        this.salary = salary;
-    }
+    int salary;
 
     public Employee(@NotNull(message = "firstName most not null") String firstName,
                     @NotNull(message = "lastName most not null") String lastName,
                     @NotNull(message = "username most not null") String username,
                     @NotNull @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#!%&*])[A-Za-z0-9@#!%&*]{8,10}$"
-            , message = "password has to be between 8 to 10 and must contain at least 1 lower and upper case and 1 digit and 1 char ") String password,
+                            , message = "password has to be between 8 to 10 and must contain at least 1 lower and upper case and 1 digit and 1 char ") String password,
                     @NotNull(message = "nationalCode most not null") String nationalCode,
                     @NotNull(message = "phoneNumber most not null") @Pattern(regexp = "^(\\+\\d{1,3}( )?)?((\\(\\d{3}\\))|\\d{3})[- .]?\\d{3}[- .]?\\d{4}$") String phoneNumber,
                     @NotNull(message = "birthDate most not null") String birthDate,
