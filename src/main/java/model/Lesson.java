5818b997de86777efc3c70c30e94d17b9d1981b4
pc <poyatajhiz59@gmail.com>
2024-04-23 13:10:48 -0800

    update Lesson model class

--- src/main/java/model/Lesson.java
+++ src/main/java/model/Lesson.java
@@ -8,9 +8,6 @@ import lombok.experimental.FieldDefaults;
 import enums.Fields;
 import org.hibernate.validator.constraints.Range;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 @Setter
 @Getter
@@ -22,18 +19,9 @@ public class Lesson extends BaseEntity<Long> {
     @NotNull(message = "name of lesson must not null")
     @Column(unique = true)
     String name;
-    @Range(min = 1,max = 4,message = "lesson count must between 1 to 4")
+    @Range(min = 1, max = 4, message = "lesson count must between 1 to 4")
     int count;
     Fields fields;
-//    @ManyToMany(mappedBy = "lessons")
-//    List<Current> currents;
-
-    public Lesson(Long aLong, String name, int count, Fields fields) {
-        super(aLong);
-        this.name = name;
-        this.count = count;
-        this.fields = fields;
-    }
 
     public Lesson(String name, int count, Fields fields) {
         this.name = name;
